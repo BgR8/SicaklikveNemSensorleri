aefa43a7bcc9e7f6c7ffcf6d2bf9b96fbb244ba7
Buğra <584254-bugrargn@users.noreply.gitlab.com>
2021-03-31 13:23:51 +0300

    DHT22 Sensörüyle Sıcaklık ve Nem Alma

---
+++ Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿
+namespace DHT22Sensor
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.comboBox_baudRate = new System.Windows.Forms.ComboBox();
+            this.comboBox_portLists = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buton_ac = new System.Windows.Forms.Button();
+            this.buton_kapat = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label_humidity = new System.Windows.Forms.Label();
+            this.label_temperature = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.comboBox_baudRate);
+            this.groupBox1.Controls.Add(this.comboBox_portLists);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 29);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(342, 152);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "ComPort Ayarları";
+            // 
+            // comboBox_baudRate
+            // 
+            this.comboBox_baudRate.FormattingEnabled = true;
+            this.comboBox_baudRate.Items.AddRange(new object[] {
+            "9600",
+            "38400",
+            "57600",
+            "115200"});
+            this.comboBox_baudRate.Location = new System.Drawing.Point(178, 84);
+            this.comboBox_baudRate.Name = "comboBox_baudRate";
+            this.comboBox_baudRate.Size = new System.Drawing.Size(121, 36);
+            this.comboBox_baudRate.TabIndex = 3;
+            this.comboBox_baudRate.Text = "57600";
+            // 
+            // comboBox_portLists
+            // 
+            this.comboBox_portLists.FormattingEnabled = true;
+            this.comboBox_portLists.Location = new System.Drawing.Point(178, 34);
+            this.comboBox_portLists.Name = "comboBox_portLists";
+            this.comboBox_portLists.Size = new System.Drawing.Size(121, 36);
+            this.comboBox_portLists.TabIndex = 2;
+            this.comboBox_portLists.DropDown += new System.EventHandler(this.comboBox_portLists_DropDown);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 28);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "BAUD RATE";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "COM PORT";
+            // 
+            // buton_ac
+            // 
+            this.buton_ac.Location = new System.Drawing.Point(360, 40);
+            this.buton_ac.Name = "buton_ac";
+            this.buton_ac.Size = new System.Drawing.Size(103, 50);
+            this.buton_ac.TabIndex = 1;
+            this.buton_ac.Text = "AÇ";
+            this.buton_ac.UseVisualStyleBackColor = true;
+            this.buton_ac.Click += new System.EventHandler(this.buton_ac_Click);
+            // 
+            // buton_kapat
+            // 
+            this.buton_kapat.Location = new System.Drawing.Point(360, 96);
+            this.buton_kapat.Name = "buton_kapat";
+            this.buton_kapat.Size = new System.Drawing.Size(103, 50);
+            this.buton_kapat.TabIndex = 1;
+            this.buton_kapat.Text = "KAPAT";
+            this.buton_kapat.UseVisualStyleBackColor = true;
+            this.buton_kapat.Click += new System.EventHandler(this.buton_kapat_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.label_humidity);
+            this.panel1.Controls.Add(this.label_temperature);
+            this.panel1.Location = new System.Drawing.Point(469, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(501, 73);
+            this.panel1.TabIndex = 2;
+            // 
+            // label_humidity
+            // 
+            this.label_humidity.AutoSize = true;
+            this.label_humidity.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label_humidity.Location = new System.Drawing.Point(265, 16);
+            this.label_humidity.Name = "label_humidity";
+            this.label_humidity.Size = new System.Drawing.Size(212, 41);
+            this.label_humidity.TabIndex = 4;
+            this.label_humidity.Text = "Nem = ... %RH";
+            // 
+            // label_temperature
+            // 
+            this.label_temperature.AutoSize = true;
+            this.label_temperature.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label_temperature.Location = new System.Drawing.Point(22, 16);
+            this.label_temperature.Name = "label_temperature";
+            this.label_temperature.Size = new System.Drawing.Size(191, 41);
+            this.label_temperature.TabIndex = 3;
+            this.label_temperature.Text = "Sıcaklık = ... °";
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(469, 113);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Legend = "Legend1";
+            series1.Name = "Sıcaklık";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.Legend = "Legend1";
+            series2.Name = "Nem";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(498, 299);
+            this.chart1.TabIndex = 3;
+            this.chart1.Text = "chart1";
+            title1.Name = "Title1";
+            title1.Text = "Sıcaklık & Nem";
+            this.chart1.Titles.Add(title1);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(598, 444);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(112, 28);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "C# Arduino";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(598, 488);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(224, 28);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "DHT22/ AM2302 Sensör";
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 28F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(982, 543);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.buton_kapat);
+            this.Controls.Add(this.buton_ac);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DHT22 Sensör İzleme";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox comboBox_baudRate;
+        private System.Windows.Forms.ComboBox comboBox_portLists;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buton_ac;
+        private System.Windows.Forms.Button buton_kapat;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label_temperature;
+        private System.Windows.Forms.Label label_humidity;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.IO.Ports.SerialPort serialPort1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace DHT22Sensor
+{
+    public partial class Form1 : Form
+    {
+        double temperature = 0, humidity = 0;
+        bool updateData = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            buton_ac.Enabled = true;
+            buton_kapat.Enabled = false;
+
+            chart1.Series["Sıcaklık"].Points.AddXY(1, 1);
+            chart1.Series["Nem"].Points.AddXY(1, 1);
+        }
+
+        private void comboBox_portLists_DropDown(object sender, EventArgs e)
+        {
+            string[] portLists = SerialPort.GetPortNames();
+            comboBox_portLists.Items.Clear();
+            comboBox_portLists.Items.AddRange(portLists);
+        }
+
+        private void buton_ac_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.PortName = comboBox_portLists.Text;
+                serialPort1.BaudRate = Convert.ToInt32(comboBox_baudRate.Text);
+                serialPort1.Open();
+
+                buton_ac.Enabled = false;
+                buton_kapat.Enabled = true;
+
+                comboBox_portLists.Enabled = false;
+                comboBox_baudRate.Enabled = false;
+
+                chart1.Series["Sıcaklık"].Points.Clear();
+                chart1.Series["Nem"].Points.Clear();
+
+                MessageBox.Show("Arduino'ya bağlantı başarılı");
+            }
+            catch (Exception error)
+            {
+
+                MessageBox.Show(error.Message);
+            }
+        }
+
+        private void buton_kapat_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.Close();
+
+                buton_ac.Enabled = true;
+                buton_kapat.Enabled = false;
+
+                comboBox_portLists.Enabled = true;
+                comboBox_baudRate.Enabled = true;
+
+                chart1.Series["Sıcaklık"].Points.Clear();
+                chart1.Series["Nem"].Points.Clear();
+
+                MessageBox.Show("Arduino'ya bağlantı kapatıldı.");
+            }
+            catch (Exception error)
+            {
+
+                MessageBox.Show(error.Message);
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                serialPort1.Close();
+            }
+            catch (Exception error)
+            {
+
+                MessageBox.Show(error.Message);
+            }
+        }
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            string data = serialPort1.ReadTo("\n");
+            Data_Parsing(data);
+            this.BeginInvoke(new EventHandler(Show_Data));
+        }
+
+        private void Show_Data(object sender, EventArgs e)
+        {
+            if (updateData == true)
+            {
+                label_temperature.Text = string.Format("Sıcaklık = {0}°C", temperature.ToString());
+                label_humidity.Text = string.Format("Nem = {0}%RH", humidity.ToString());
+
+                chart1.Series["Temperature"].Points.Add(temperature);
+                chart1.Series["Humidity"].Points.Add(humidity);
+            }
+        }
+
+        private void Data_Parsing(string data)
+        {
+            sbyte indexOf_startDataCharacter = (sbyte)data.IndexOf("@");
+            sbyte indexOfA = (sbyte)data.IndexOf("A");
+            sbyte indexOfB = (sbyte)data.IndexOf("B");
+
+            if(indexOfA!=-1 && indexOfB!=-1 && indexOf_startDataCharacter!=-1)
+            {
+                try
+                {
+                    string str_temperature = data.Substring(indexOf_startDataCharacter+1, (indexOfA - indexOf_startDataCharacter) - 1);
+                    string str_humidty = data.Substring(indexOfA + 1, (indexOfB-indexOfA)-1 );
+
+                    temperature = Convert.ToDouble(str_temperature);
+                    humidity = Convert.ToDouble(str_humidty);
+
+                    updateData = true;
+
+
+                }
+                catch (Exception)
+                {
+
+                }
+            }
+            else
+            {
+                updateData = false;
+            }
+        }
+    }
+}
